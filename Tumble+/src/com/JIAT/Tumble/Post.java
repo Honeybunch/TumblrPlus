241e892c6af5a5320b776204dfb43ed88713459a
Arsen Tufankjian <ArsenTufSc2@gmail.com>
2013-08-02 11:45:03 -0400

    Started Post Displaying System
    Added Post.java and populated it a bit. It should end up like a really
    complex data structure.

--- Tumble+/src/com/JIAT/Tumble/Post.java
+++ Tumble+/src/com/JIAT/Tumble/Post.java
@@ -1,5 +1,7 @@
 package com.JIAT.Tumble;
 
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Arsen
@@ -8,4 +10,26 @@ package com.JIAT.Tumble;
  * To change this template use File | Settings | File Templates.
  */
 public class Post {
+
+    //General Post data
+    private String blogName;
+    private String id;
+    private String postUrl;
+    private String type;
+    private String date;
+    private String timestamp;
+    private String state;
+    private String format;
+    private String reblogKey;
+    private ArrayList<String> tags;
+    private String shortUrl;
+    private boolean followed;
+    private ArrayList<String> highlighted;
+    private boolean liked;
+    private String noteCount;
+    private String sourceUrl;
+    private String sourceTitle;
+    private String title;
+    private String body;
+
 }
